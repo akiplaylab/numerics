724e946c5262e2b1378b46a3c6e7b72233e8d7d0
aki <aki.grosjean@gmail.com>
2025-07-17 21:50:48 +0900

    Add combinations

---
+++ AkiPlayLab.Numerics.Tests/CombinatorialEnumerableTests.cs
@@ -0,0 +1,31 @@
+﻿namespace AkiPlayLab.Numerics.Tests;
+
+[TestClass()]
+public class CombinatorialEnumerableTests
+{
+    [TestMethod()]
+    public void CombinationsTest()
+    {
+        int[] source = [1, 2, 3, 4, 5];
+        int k = 3;
+        int[][] expected =
+        [
+            [1, 2, 3],
+            [1, 2, 4],
+            [1, 2, 5],
+            [1, 3, 4],
+            [1, 3, 5],
+            [1, 4, 5],
+            [2, 3, 4],
+            [2, 3, 5],
+            [2, 4, 5],
+            [3, 4, 5]
+        ];
+        var result = CombinatorialEnumerable.Combinations(source, k).ToArray();
+        Assert.AreEqual(expected.Length, result.Length);
+        for (int i = 0; i < expected.Length; i++)
+        {
+            CollectionAssert.AreEqual(expected[i], result[i]);
+        }
+    }
+}
---
+++ AkiPlayLab.Numerics/CombinatorialEnumerable.cs
@@ -0,0 +1,28 @@
+﻿namespace AkiPlayLab.Numerics;
+
+public static class CombinatorialEnumerable
+{
+    public static IEnumerable<T[]> Combinations<T>(T[] source, int k)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(k);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(k, source.Length);
+
+        if (k == 0)
+        {
+            yield return Array.Empty<T>();
+            yield break;
+        }
+
+        int[] indices = [.. Enumerable.Range(0, k)];
+        while (true)
+        {
+            yield return indices.Select(i => source[i]).ToArray();
+            int i;
+            for (i = k - 1; i >= 0 && indices[i] == source.Length - k + i; i--) ;
+            if (i < 0) break;
+            indices[i]++;
+            for (int j = i + 1; j < k; j++)
+                indices[j] = indices[j - 1] + 1;
+        }
+    }
+}
